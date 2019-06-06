ee0d442eeb5e0a6cd496552d45c5fea6b830d536
Juan Diego Villalobos Quirós <jvillalobos@icinetic.com>
2019-06-06 17:09:04 +0200

    juegodelavida.c actualizado.

--- juegodelavida.c
+++ juegodelavida.c
@@ -1,39 +1,40 @@
 #include <stdio.h>
-#include <stdlib.h>
-#include <stdbool.h>
 #include "juegodelavida.h"
 
-void gol_init(bool mundo[TAM_X][TAM_Y]) {
+void gol_init(struct jdlv *jdlv) {
+
+	jdlv->mundo = 0;
 	for (int i = 0; i < TAM_X; i++){
 		for (int j = 0; j < TAM_Y; j++){
-			mundo[i][j] = false;
+			jdlv->mundos[jdlv->mundo][i][j] = false;
 		}
 	}
-	mundo[0][1] = true;
-	mundo[1][2] = true;
-	mundo[2][0] = true;
-	mundo[2][1] = true;
-	mundo[2][2] = true;      
+	jdlv->mundos[jdlv->mundo][0][1] = true;
+	jdlv->mundos[jdlv->mundo][1][2] = true;
+	jdlv->mundos[jdlv->mundo][2][0] = true;
+	jdlv->mundos[jdlv->mundo][2][1] = true;
+	jdlv->mundos[jdlv->mundo][2][2] = true;      
 }
 
-void gol_print(bool mundo[TAM_X][TAM_Y]) {
+void gol_print(struct jdlv *jdlv) {
 	for (int i = 0; i < TAM_X; i++) {
 		for (int j = 0; j < TAM_Y; j++) {
-			printf("%s", mundo[i][j] ? " #" : " .");
+			printf("%s", jdlv->mundos[jdlv->mundo][i][j] ? " #" : " .");
 		}
 		printf("\n");
 	}
 	printf("\n");
 }
 
-void gol_step(bool mundoa[TAM_X][TAM_Y], bool mundob[TAM_X][TAM_Y]) {
+void gol_step(struct jdlv *jdlv) {
 	for (int i = 0; i < TAM_X; i++) {
 		for (int j = 0; j < TAM_Y; j++) {
 			int n = gol_count_neighbors(mundoa, i, j);
-			mundob[i][j] = (mundoa[i][j] && n == 2) || n == 3;
+			jdlv->mundos[!jdlv->mundo][i][j] =
+				(jdlv->mundos[jdlv->mundo][i][j] && n == 2) || n == 3;
 		}
 	}
-	gol_copy(mundoa, mundob);
+	jdlv->mundo = !jdlv->mundo;
 }
 
 int gol_count_neighbors(bool mundo[GOL_SIZE_X][GOL_SIZE_Y], int x, int y) {
