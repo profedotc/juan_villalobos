c6784a19d359ab099ed83b95e1a8c191c45aeff9
Juan Diego Villalobos Quirós <jvillalobos@icinetic.com>
2019-06-08 19:15:48 +0200

    Tarea 2.1
    He arreglado el archivo main.c, los errores.

--- main.c
+++ main.c
@@ -9,17 +9,18 @@
 #define TAM_X 5
 #define TAM_Y 5
 
-void gol_init(bool[TAM_X][TAM_Y]);
-void gol_print(bool[TAM_X][TAM_Y]);
-void gol_step(bool[TAM_X][TAM_Y], bool[TAM_X][TAM_Y]);
-int gol_count_neighbors(bool[TAM_X][TAM_Y], int, int);
-bool gol_get_cell(bool[TAM_X][TAM_Y], int, int);
+void gol_init(bool mundo[TAM_X][TAM_Y]);
+void gol_print(bool mundo[TAM_X][TAM_Y]);
+void gol_step(bool mundoa[TAM_X][TAM_Y], bool mundob[TAM_X][TAM_Y]);
+int gol_count_neighbors(bool mundo[TAM_X][TAM_Y], int x, int y);
+bool gol_get_cell(bool mundo[TAM_X][TAM_Y], int x, int y);
+void gol_copy(bool dst[TAM_X][TAM_Y], bool src[TAM_X][TAM_Y]);
 
-int main() {
+int main()
+{
 	int i = 0;
 	bool mundos[2][TAM_X][TAM_Y];
 	unsigned int mundo = 0;
-
 	gol_init(mundos[mundo]);
 	do {
 		printf("\033cIteration %d\n", i++);
@@ -30,17 +31,17 @@ int main() {
 	return EXIT_SUCCESS;
 }
 
-void gol_init(bool mundo[TAM_X][TAM_Y]) {
-	for (int i = 0; i < TAM_X; i++){
-		for (int j = 0; j < TAM_Y; j++){
+void gol_init(bool mundo[TAM_X][TAM_Y]){
+	for (int i = 0; i < TAM_X; i++)
+		for (int j = 0; j < TAM_Y; j++)
 			mundo[i][j] = false;
-		}
-	}
+
+	// Glider
 	mundo[0][1] = true;
 	mundo[1][2] = true;
 	mundo[2][0] = true;
 	mundo[2][1] = true;
-	mundo[2][2] = true;      
+	mundo[2][2] = true;
 }
 
 void gol_print(bool mundo[TAM_X][TAM_Y]) {
@@ -60,7 +61,6 @@ void gol_step(bool mundoa[TAM_X][TAM_Y], bool mundob[TAM_X][TAM_Y]) {
 			mundob[i][j] = (mundoa[i][j] && n == 2) || n == 3;
 		}
 	}
-	gol_copy(mundoa, mundob);
 }
 
 int gol_count_neighbors(bool mundo[TAM_X][TAM_Y], int x, int y) {
@@ -77,15 +77,14 @@ int gol_count_neighbors(bool mundo[TAM_X][TAM_Y], int x, int y) {
 }
 
 bool gol_get_cell(bool mundo[TAM_X][TAM_Y], int x, int y) {
-	if (x >= TAM_X) {
-        x = 0;
-    } else if (x < 0) {
-            x = TAM_X - 1;
-    } 
-    if (y >= TAM_Y) {
-            y = 0;
-    } else if (y < 0) {
-            y = TAM_Y - 1;
-    }
+	if (x >= TAM_X)
+		x = 0;
+	else if (x < 0)
+		x = TAM_X - 1;
+
+	if (y >= TAM_Y)
+		y = 0;
+	else if (y < 0)
+		y = TAM_Y - 1;
 	return mundo[x][y];
 }
