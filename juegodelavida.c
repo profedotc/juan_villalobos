3d23bd2a2850e8320bd687b1abc8297f5dec70f3
Juan Diego Villalobos Quirós <jvillalobos@icinetic.com>
2019-06-08 21:12:39 +0200

    Tarea 3.1
    Más refactorización de código.

--- juegodelavida.c
+++ juegodelavida.c
@@ -2,15 +2,11 @@
 
 #include <stdio.h>
 
-void gol_init(struct jdlv *jdlv)
-{
+void gol_init(struct jdlv *jdlv) {
 	jdlv->mundo = 0;
-
 	for (int i = 0; i < TAM_X; i++)
 		for (int j = 0; j < TAM_Y; j++)
 			jdlv->mundos[jdlv->mundo][i][j] = false;
-
-	// Glider
 	jdlv->mundos[jdlv->mundo][0][1] = true;
 	jdlv->mundos[jdlv->mundo][1][2] = true;
 	jdlv->mundos[jdlv->mundo][2][0] = true;
@@ -18,8 +14,7 @@ void gol_init(struct jdlv *jdlv)
 	jdlv->mundos[jdlv->mundo][2][2] = true;
 }
 
-void gol_print(const struct jdlv *jdlv)
-{
+void gol_print(const struct jdlv *jdlv) {
 	for (int i = 0; i < TAM_X; i++) {
 		for (int j = 0; j < TAM_Y; j++) {
 			printf("%s", jdlv->mundos[jdlv->mundo][i][j] ?
@@ -30,48 +25,39 @@ void gol_print(const struct jdlv *jdlv)
 	printf("\n");
 }
 
-void gol_step(struct jdlv *jdlv)
-{
+void gol_step(struct jdlv *jdlv) {
 	for (int i = 0; i < TAM_X; i++) {
 		for (int j = 0; j < TAM_Y; j++) {
-			int n = gol_count_neighbors(gol, i, j);
+			int n = gol_count_neighbors(jdlv, i, j);
 			jdlv->mundos[!jdlv->mundo][i][j] =
 				(jdlv->mundos[jdlv->mundo][i][j] && n == 2)
 				|| n == 3;
 		}
 	}
-
 	jdlv->mundo = !jdlv->mundo;
 }
 
-int gol_count_neighbors(const struct jdlv *jdlv, int x, int y)
-{
+int gol_count_neighbors(const struct jdlv *jdlv, int x, int y) {
 	int count = 0;
-
-	count += gol_get_cell(gol, x - 1, y + 1);
-	count += gol_get_cell(gol, x - 0, y + 1);
-	count += gol_get_cell(gol, x + 1, y + 1);
-	count += gol_get_cell(gol, x - 1, y + 0);
-	count += gol_get_cell(gol, x + 1, y + 0);
-	count += gol_get_cell(gol, x - 1, y - 1);
-	count += gol_get_cell(gol, x - 0, y - 1);
-	count += gol_get_cell(gol, x + 1, y - 1);
-
+	count += gol_get_cell(jdlv, x - 1, y + 1);
+	count += gol_get_cell(jdlv, x - 0, y + 1);
+	count += gol_get_cell(jdlv, x + 1, y + 1);
+	count += gol_get_cell(jdlv, x - 1, y + 0);
+	count += gol_get_cell(jdlv, x + 1, y + 0);
+	count += gol_get_cell(jdlv, x - 1, y - 1);
+	count += gol_get_cell(jdlv, x - 0, y - 1);
+	count += gol_get_cell(jdlv, x + 1, y - 1);
 	return count;
 }
 
-bool gol_get_cell(const struct jdlv *jdlv, int x, int y)
-{
-	// Fix coords
+bool gol_get_cell(const struct jdlv *jdlv, int x, int y) {
 	if (x >= TAM_X)
 		x = 0;
 	else if (x < 0)
 		x = TAM_X - 1;
-
 	if (y >= TAM_Y)
 		y = 0;
 	else if (y < 0)
 		y = TAM_Y - 1;
-
 	return jdlv->mundos[jdlv->mundo][x][y];
 }
